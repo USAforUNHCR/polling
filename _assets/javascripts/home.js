3002232f268ae7cff01e8712b76a15e48e397be6
Chris Wallwork <chris@unrefgugees.org>
2015-12-16 15:44:05 -0500

    changed to live creds

--- _assets/javascripts/home.js
+++ _assets/javascripts/home.js
@@ -3,11 +3,28 @@ var DATA = [
   {category: "HIVE", fact: "How many people support refugee resettlement?:", number: "60%", color: "red"}
 ]
 
-var dataObjects = {categories: ['unhcr','hive']};
+var dataObjects = {categories: ['unhcr','hive','refugee']};
+
+var data = {
+  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri','Sat','Sun'],
+  series: [
+    [5, 2, 4, 2, 1, 0, 0],[0, 1, 2, 3.5, 4, 4.5, 4.5]
+  ]
+}
 
 $(document).ready(function(){
+  $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: 400
+  })
   createDataObjects();
   showListeners();
+  new Chartist.Line('#line-chart',data,
+    {axisY: {
+      onlyInteger: true
+    }
+  });
+  biPolar();
 })
 
 function showListeners(){
@@ -47,17 +64,39 @@ dataObjects.toggle = function(category){
 
     if(this.categories[i] != category){
     var  elements = this[this.categories[i]];
-    elements.collapse('hide');
+    elements.fadeOut('slow');
     }
     else if(this.categories[i] === category){
       var  elements = this[this.categories[i]];
-      elements.collapse('show');
+      elements.fadeIn('slow');
     }
   } 
 }
 
 dataObjects.showAll = function(){
   for(var i=0 ; i < this.categories.length ; i++){
-    this[this.categories[i]].collapse('show');
+    this[this.categories[i]].fadeIn('slow');
   }
+}
+
+function biPolar(){
+  var data = {
+  labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+  series: [
+    [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+  ]
+};
+
+var options = {
+  high: 10,
+  low: -10,
+  axisX: {
+    labelInterpolationFnc: function(value, index) {
+      return index % 2 === 0 ? value : null;
+    }
+  }
+};
+
+new Chartist.Bar('#bi-polar', data, options);
+
 }
